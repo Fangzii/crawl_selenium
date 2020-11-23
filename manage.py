63e9dc7408f7ea9c94fbf1b35ecd855c5d488fe6
fangzicheng <fangzicheng@eastwestec.com>
2020-11-23 10:04:18 +0800

    重释逻辑更新

--- manage.py
+++ manage.py
@@ -8,6 +8,18 @@ try:
 except ImportError:
     pass
 
+
+def continue_load():
+    reload = raw_input('已经抓取%s 条数据是否继续(Y/N):' % len(crawl.all_data))
+    if str(reload) == 'Y':
+        try:
+            crawl.selenium_page()
+        except Exception as e:
+            continue_load()
+    else:
+        pass
+
+
 if __name__ == '__main__':
     try:
         crawl = Crawl(DEFAULT_URL, DEFAULT_ID, CHROMEDRIVER_URL, XLWT_URL)
@@ -15,11 +27,7 @@ if __name__ == '__main__':
         crawl.run()
         print('抓取完成, 已经抓取%s 条数据' % len(crawl.all_data))
     except Exception as e:
-        reload = raw_input('已经抓取%s 条数据是否继续(Y/N):' % len(crawl.all_data))
-        if str(reload) == 'Y':
-            crawl.run()
-        else:
-            pass
+        continue_load()
 
     title = raw_input('是否输出为表格并输入名称 ( 关闭 ctrl + c ): ')
     if len(title):
